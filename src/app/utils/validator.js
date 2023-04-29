47625b00f4bdb32a8a242ff7317ee3fba8c3fb67
Алексей@HP <https://github.com/AkselRus>
2023-04-29 13:46:14 +0300

    task 1 form 1

---
+++ src/app/utils/validator.js
@@ -0,0 +1,46 @@
+export function validator(data, config) {
+    const error = {};
+    function validate(validMethod, data, config) {
+        let statusValidate;
+        switch (validMethod) {
+        case "isRequired":
+            statusValidate = data.trim() === "";
+            break;
+        case "isEmail": {
+            const emailRegExp = /^\S+@\S+\.\S+$/g;
+            statusValidate = !emailRegExp.test(data);
+            break;
+        }
+        case "isPasswordSymbol": {
+            const passwordRegExp = /[A-Z]+/g;
+            statusValidate = !passwordRegExp.test(data);
+            break;
+        }
+        case "isPasswordDigital": {
+            const passwordDigit = /\d+/g;
+            statusValidate = !passwordDigit.test(data);
+            break;
+        }
+        case "min": {
+            statusValidate = data.length < config.value;
+            break;
+        }
+        default:
+            break;
+        }
+        if (statusValidate) return config.message;
+    }
+    for (const fieldName in data) {
+        for (const validMethod in config[fieldName]) {
+            const err = validate(
+                validMethod,
+                data[fieldName],
+                config[fieldName][validMethod]
+            );
+            if (err && !error[fieldName]) {
+                error[fieldName] = err;
+            };
+        }
+    }
+    return error;
+}
