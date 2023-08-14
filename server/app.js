89dacaf890d7b84a567c4b342580ec323433f504
Алексей@HP <https://github.com/AkselRus>
2023-08-14 20:44:21 +0300

    init app backend to node.js

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const chalk = require("chalk");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = config.get("port") ?? 8080;
+
+// if (process.env.NODE_ENV === "production") {
+//     console.log("Production");
+// } else {
+//     console.log("Development");
+// }
+
+async function start() {
+    try {
+        await mongoose.connect(config.get("mongoUrl"));
+        app.listen(PORT, () =>
+            console.log(
+                chalk.green(`Server has been started on port ${PORT}...`)
+            )
+        );
+    } catch (error) {
+        console.log(chalk.red(error.message));
+        process.exit(1);
+    }
+}
+
+start();
